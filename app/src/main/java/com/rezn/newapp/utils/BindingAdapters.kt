8141ae0a6bcab51fb887c693dad7430032b8f47f
rezn4 <rezn.dm@gmail.com>
2019-09-20 15:06:38 +0300

    Unused packages removed

--- app/src/main/java/com/rezn/newapp/utils/BindingAdapters.kt
+++ app/src/main/java/com/rezn/newapp/utils/BindingAdapters.kt
@@ -1,14 +1,9 @@
 package com.rezn.newapp.utils
 
 import android.widget.ImageView
-import android.widget.ListAdapter
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.rezn.newapp.R
-import com.squareup.picasso.Picasso
 
 object BindingAdapters {
     @BindingAdapter("pl_picture")
